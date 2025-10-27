6fcd68e10e69c561e29580652ddebc8d706e834b
duyacquy <duytran2552005@gmail.com>
2025-10-27 18:26:38 +0700

    Add UCI and pubmed

--- classification/config.py
+++ classification/config.py
@@ -8,7 +8,9 @@ example_name = {
     "Duyacquy/Single_label_medical_abstract": 'medical_abstract', 
     "Duyacquy/Legal_text": 'case_text', 
     "Duyacquy/Ecommerce_text": 'text', 
-    "Duyacquy/Stack_overflow_question": 'Text'
+    "Duyacquy/Stack_overflow_question": 'Text',
+    "Duyacquy/Pubmed_20k": 'abstract_text',
+    "Duyacquy/UCI_drug": 'review'
 } # Done
 
 concepts_from_labels = {'SetFit/sst2': ["negative","positive"], "fancyzhx/yelp_polarity": ["negative","positive"], "fancyzhx/ag_news": ["World", "Sports", "Business", "Sci/Tech"], "fancyzhx/dbpedia_14": [
@@ -26,10 +28,13 @@ concepts_from_labels = {'SetFit/sst2': ["negative","positive"], "fancyzhx/yelp_p
         "album",
         "film",
         "written work"
-    ], "Duyacquy/Single_label_medical_abstract": ["neoplasms", "digestive_system_diseases", "nervous_system_diseases", "cardiovascular_diseases", "general_pathological_diseases"], 
+    ], 
+    "Duyacquy/Single_label_medical_abstract": ["neoplasms", "digestive_system_diseases", "nervous_system_diseases", "cardiovascular_diseases", "general_pathological_diseases"], 
     "Duyacquy/Legal_text": ["applied", "cited", "considered", "followed", "referred to"], 
     "Duyacquy/Ecommerce_text": ["Household", "Books", "Electronics", "Clothing & Accessories"], 
-    "Duyacquy/Stack_overflow_question": ["HQ", "LQ_EDIT", "LQ_CLOSE"]
+    "Duyacquy/Stack_overflow_question": ["HQ", "LQ_EDIT", "LQ_CLOSE"],
+    "Duyacquy/Pubmed_20k": ["BACKGROUND", "OBJECTIVE", "METHODS", "RESULTS", "CONCLUSIONS"],
+    "Duyacquy/UCI_drug": ["1", "5", "10"]
 } # Done
 
 class_num = {
@@ -40,7 +45,9 @@ class_num = {
     "Duyacquy/Single_label_medical_abstract": 5, 
     "Duyacquy/Legal_text": 5, 
     "Duyacquy/Ecommerce_text": 4, 
-    "Duyacquy/Stack_overflow_question": 3
+    "Duyacquy/Stack_overflow_question": 3,
+    "Duyacquy/Pubmed_20k": 5, 
+    "Duyacquy/UCI_drug": 3
 } # Done
 
 # Config for Roberta-Base baseline
@@ -56,7 +63,9 @@ concept_set = {
     "Duyacquy/Single_label_medical_abstract": concepts.med_abs, 
     "Duyacquy/Legal_text": concepts.legal, 
     "Duyacquy/Ecommerce_text": concepts.ecom,
-    "Duyacquy/Stack_overflow_question": concepts.stackoverflow
+    "Duyacquy/Stack_overflow_question": concepts.stackoverflow,
+    "Duyacquy/Pubmed_20k": concepts.pubmed,
+    "Duyacquy/UCI_drug": concepts.drug
 }
 
 cbl_epochs = {
@@ -67,7 +76,9 @@ cbl_epochs = {
     "Duyacquy/Single_label_medical_abstract": 10, 
     "Duyacquy/Legal_text": 10, 
     "Duyacquy/Ecommerce_text": 10, 
-    "Duyacquy/Stack_overflow_question": 10
+    "Duyacquy/Stack_overflow_question": 10,
+    "Duyacquy/Pubmed_20k": 10, 
+    "Duyacquy/UCI_drug": 10
 }
 
 dataset_config = {
@@ -99,14 +110,6 @@ dataset_config = {
         "text_column": "text",
         "label_column": "label"
     },
-    "pietrolesci/pubmed-200k-rct": {
-        "text_column": "text",
-        "label_column": "labels"
-    },
-    "dd-n-kk/uci-drug-review-cleaned": {
-        "text_column": "review",
-        "label_column": "rating"
-    },
     "Duyacquy/Legal_text": {
         "text_column": "case_text",
         "label_column": "case_outcome"
@@ -118,5 +121,13 @@ dataset_config = {
     "Duyacquy/Stack_overflow_question": {
         "text_column": "Text",
         "label_column": "Y"
-    }
+    },
+    "Duyacquy/Pubmed_20k": {
+        "text_column": "abstract_text",
+        "label_column": "target"
+    },
+    "Duyacquy/UCI_drug": {
+        "text_column": "review",
+        "label_column": "rating"
+    },
 }
